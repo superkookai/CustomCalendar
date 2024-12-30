0f31d26024cd338585853a1d9b1e373421b51d46
Weerawut <weerawut@live.com>
2024-12-30 09:26:32 +0700

    Initial Commit

---
+++ CustomCalendar/CustomCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCalendarApp.swift
+//  CustomCalendar
+//
+//  Created by Weerawut Chaiyasomboon on 30/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct CustomCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
